8e3534a5837e399b68ad5888535f54a106105435
Sparrow8104 <anchalsingh8102004@gmail.com>
2025-09-18 21:00:43 +0530

    Added File Sevice impl

---
+++ src/main/java/com/movieflix/movieApi/service/FileServiceImpl.java
@@ -0,0 +1,21 @@
+package com.movieflix.movieApi.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class FileServiceImpl implements FileService {
+    @Override
+    public String uploadFile(String path, MultipartFile file) throws IOException {
+        return "";
+    }
+
+    @Override
+    public InputStream getSourceFile(String path, String name) throws FileNotFoundException {
+        return null;
+    }
+}
